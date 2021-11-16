6c4ddc133223e2d5a9c20a8285dbaefc05f0a583
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-16 18:43:16 +0100

    Create sketch.js

---
+++ code/task-03_polar-corsys-1/sketch.js
@@ -0,0 +1,30 @@
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+let angle = 0;
+let r = 300;
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+  frameRate(100);
+}
+
+function draw() {
+  background(0, 10);
+  translate(200, 200);
+  noFill();
+  strokeWeight(2);
+  stroke(random(0,255),random(0,255),random(0,255));
+  let x = r * cos(angle);
+  let y = r * sin(angle);
+  rect(x,y, random(0,10), random(0,100));
+
+  angle += 0.05;
+  r -= 0.4;
+
+  if (r < -300){
+    r = 160;
+  }
+}
+
+
