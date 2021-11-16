93401b4947f3c2d656b691ce7a95f52043a0a099
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-16 18:43:39 +0100

    Create sketch.js

---
+++ code/task-02_nr28/sketch.js
@@ -0,0 +1,41 @@
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+}
+
+function draw() {
+  noLoop();
+  background(255);
+  
+  noFill();
+  stroke("black");
+
+  let radius = 50;
+  
+  circle(sketchWidth / 2, sketchHeight / 2, 50)
+
+  let maxCount = 6;
+  for (let circleCount = 0; circleCount < maxCount; circleCount += 1) {
+      let theta = (Math.PI / 180) * (360 / maxCount * circleCount);
+
+      let x = radius * cos(theta);
+      let y = radius * sin(theta);
+
+      circle(
+        x + sketchWidth / 2,
+        y + sketchHeight / 2,
+        radius
+        );
+    }
+  }
+
+
+
+
+
+
+
+
