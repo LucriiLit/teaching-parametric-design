ab567abea9bb8b9f3ff7452fdbb612cd14289c0c
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-24 10:00:34 +0100

    Create sketch.js

---
+++ code/task-04.2/sketch.js
@@ -0,0 +1,67 @@
+const sketchHeight = 400;
+const sketchWidth = 400;
+
+let r = 150;
+
+//almost useless Array
+let translates = [200,0,100,400,50,150,250,300,350];
+let swappX = 0;
+let swappY = 0;
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+  frameRate(30);
+}
+
+function draw() {
+  background(255); //zweite Ziffer = Transparenz zum vorherigen Durchlauf
+  translate(translates[swappX], translates[swappY]);
+  noFill();
+
+  let increment = map(mouseX, 100, width, 0.01, PI); //X-Wert der Maus im Browser auslesen
+
+  let kreise = 4;
+
+  //Schlaufe für Menge der Kreise bei nicht Transparenz (für SVG)
+  for (let g = 1; g <= kreise; g += 1) {
+
+    beginShape();  
+      for (let a = 0; a < TWO_PI; a += 0.2) { //Schlaufe zum generieren des Polaren Koordinaten Systems
+        let r1 = r + random(-10,10);
+        let x = r1 * cos(a);
+        let y = r1 * sin(a);
+
+        //Kreise generieren
+        strokeWeight(1);
+        stroke("black"); //Random Blaufarben
+        circle(x,y, random(0,10));
+
+        //Vertices generieren (Striche zwischen Kreisen)
+        strokeWeight(0.5);
+        stroke("red"); //Random Gelbfarben
+        vertex(x,y)
+
+        //Radius abhängig von Maus ändern
+        r = increment*100;
+        print(increment);
+
+
+      }
+    endShape(CLOSE);
+  }
+}
+
+function mouseClicked() {
+  save(sketch);
+}
+
+
+
+
+
+
+/*
+HA
+Sinnvolles Objekt & Array mit einbauen
+*/
+
