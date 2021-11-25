b12d05ebdc5876e54ba9808fb2e603b81f26cd8e
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-25 01:05:18 +0100

    Update sketch.js

--- code/task-04.2/sketch.js
+++ code/task-04.2/sketch.js
@@ -1,7 +1,7 @@
 const sketchHeight = 400;
 const sketchWidth = 400;
 
-let r = 120;
+let r = 56;
 
 //almost useless Array
 let translates = [200,0,100,400,50,150,250,300,350];
@@ -23,7 +23,7 @@ function draw() {
 
   let increment = map(mouseX, 100, width, 0.01, PI); //X-Wert der Maus im Browser auslesen
 
-  let kreise = 4;
+  let kreise = 20;
 
   //Schlaufe für Menge der Kreise bei nicht Transparenz (für SVG)
   for (let g = 1; g <= kreise; g += 1) {
@@ -40,12 +40,12 @@ function draw() {
 
         //Vertices generieren (Striche zwischen Kreisen)
         stroke(random(100,200)); //Random Gelbfarben
-        vertex(x,y)
+        vertex(x,y);
 
         //Radius abhängig von Maus ändern
         print(increment);
       }
-    r = r + 10;
+    r = r + 2;
     endShape(CLOSE);
   }
 }
