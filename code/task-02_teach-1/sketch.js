1919c58435c1d0a4598792893bc6c21047238ea3
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-25 01:06:03 +0100

    Update sketch.js

--- code/task-02_teach-1/sketch.js
+++ code/task-02_teach-1/sketch.js
@@ -10,31 +10,37 @@ let trueFalse = true; // false
 
 function setup() {
   createCanvas(400, 400);
-  frameRate(10);
   colorValue = random(0, 255);
   xValue = 0;
 
 }
 
 function draw() {
-  noLoop();
   background(colorValue);
-  
   fill("xValue");
 
-  for (let y = 0; y < 10; y += 1) {
-    for (let x = 0; x < 10; x += 1) {
-      fill(colorValue, 25, random(0, 255))
-      circle(
-        100 + y * 20,
-        100 + x * 20,
-        y * x
-      );
-      // xValue += 5;
+  for(let p = 0; p < 20; p += 2){
+    if (p == 20){
+      p = 0;
     }
-  }
+    for (let y = 0; y < 10; y += 1) {
+      for (let x = 0; x < 10; x += 1) {
+        fill(colorValue, 25, random(0, 255))
+        circle(
+          100 + y * 20,
+          100 + x * 20 ,
+          x * y
+        );
+      }
+    }
   noFill();
-  // curve(5, 26, 5, 26, 73, 24, 73, 61);
+  }
+
+  // for(let p = 0; p < 20; p += 2){
+  //   circle(200, 200, p);
+  //   rotate(PI / 180 * p);
+  //   rect(50, 50, 50, 50)
+  // }
 }
 
 
