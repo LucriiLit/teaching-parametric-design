818f2cccb7e358136842133a5b62bf71c1f0e7b7
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-25 01:05:41 +0100

    Create sketch.js

---
+++ code/task-03_osaft/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+	background(0);
+	noFill();
+
+	let linienanzahl = 10;
+	let wellen = 200;
+
+  let time = millis() / 5000;
+
+	for (j = 0; j < linienanzahl; j++) {
+		let farbe = j / linienanzahl*360;
+
+		beginShape();
+		stroke(200, 200, farbe);
+
+		for (i = 0; i < wellen; i++) {
+
+			let wellenabstand = (j / 10 + time);
+			let py = i / (50 + j);
+
+			let y = noise(wellenabstand, py) * height;
+
+			let x = i / (wellen - 1) * width;
+
+			vertex(x, y);
+		}
+		endShape();
+	}
+}
