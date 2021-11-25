ece4ecd02d49fc851ae5197721ab657b0bcb8ff7
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-11-25 01:06:01 +0100

    Update sketch.js

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -2,6 +2,8 @@ const sketchHeight = 400;
 const sketchWidth = 400;
 
 let r = 150;
+let kreise = 8;
+
 
 //almost useless Array
 let translates = [200,0,100,400,50,150,250,300,350];
@@ -9,39 +11,48 @@ let swappX = 0;
 let swappY = 0;
 
 function setup() {
-  createCanvas(sketchWidth, sketchHeight);
-  frameRate(60);
+  createCanvas(sketchWidth, sketchHeight, SVG);
+  frameRate(30);
+  // noLoop();
 }
 
 function draw() {
-  background(0,10); //zweite Ziffer = Transparenz zum vorherigen Durchlauf
+  background(0); //zweite Ziffer = Transparenz zum vorherigen Durchlauf
   translate(translates[swappX], translates[swappY]);
   noFill();
 
-  let increment = map(mouseX, 100, width, 0.01, PI); //Ort der Maus im Browser auslesen
+  let increment = map(mouseX, 100, width, 0.01, PI); //X-Wert der Maus im Browser auslesen
 
-  beginShape(); //erstellen der Vertex shapes
-    for (let a = 0; a < TWO_PI; a += 0.2) {
-      let r1 = r + random(-10,10);
-      let x = r1 * cos(a);
-      let y = r1 * sin(a);
 
-      //Kreise generieren
-      strokeWeight(1);
-      stroke(random(0,255), random(0,255), 255); //Random Blaufarben
-      circle(x,y, random(0,10));
+  //Schlaufe für Menge der Kreise bei nicht Transparenz (für SVG)
+  for (let g = 1; g <= kreise; g += 1) {
 
-      //Vertices generieren (Striche zwischen Kreisne)
-      strokeWeight(0.5);
-      stroke(0, random(0,255), 255); //Random Gelbfarben
-      vertex(x,y);
+    beginShape();  
+      for (let a = 0; a < TWO_PI; a += 0.2) { //Schlaufe zum generieren des Polaren Koordinaten Systems
+        let r1 = r + random(-10,10);
+        let x = r1 * cos(a);
+        let y = r1 * sin(a);
 
-      //Radius abhängig von Maus ändern
-      r = increment*100;
-      print(increment);
+        //Kreise generieren
+        strokeWeight(1);
+        stroke(random(0,255), random(0,255), 255); //Random Blaufarben
+        circle(x,y, random(0,10));
 
-    }
-  endShape(CLOSE);  
+        //Vertices generieren (Striche zwischen Kreisen)
+        strokeWeight(0.5);
+        stroke(0, random(0,255), 255); //Random Gelbfarben
+        curveVertex(x,y);
+
+        //Radius abhängig von Maus ändern
+        r = increment*100;
+        print(increment);
+      }
+    endShape(CLOSE);
+  }
+}
+
+function mouseClicked() {
+  // save();
 }
 
 
