b036e956573951127a21199a450d463da3a7ded5
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2021-12-01 19:08:38 +0100

    Create index.js

---
+++ code/task-05-teaching/index.js
@@ -0,0 +1,48 @@
+// importing jscad functions
+const jscad = require('@jscad/modeling')
+
+// creating shortcuts
+const {line, arc, circle, ellipse, rectangle, cube, sphere, cylinder, cuboid, roundedCuboid, geodesicSphere, ellipsoid, roundedCylinder, cylinderElliptic, torus} = jscad.primitives;
+const {extrudeRectangular, extrudeLinear, extrudeRotate} = jscad.extrusions;
+const {colorize, colorNameToRgb} = jscad.colors;
+const {union, subtract, intersect, scission} = jscad.booleans;
+const {translate, rotate, scale, center, align} = jscad.transforms;
+
+
+// this is where we draw our shapes
+const main = () => {
+
+    // Erstellen von 2 Variablen (const) die einen Cylinder und ein Quader zugeshrieben bekommen
+    const coolerCyli = translate([0, 0, -20], cylinder({ radius: 10 }));
+    const coolerBla = cuboid({ size: [100, 100, 1] });
+
+
+    // Erstellen eines Arrays in den ein Cylinder mit speziellen Werten gepusht wird
+    const objects = [];
+    objects.push( translate([0, 0, -10], cylinder({ radius: 20, segments: 7 })) );
+
+    // Erstellen eines Arrays und einer Variablen mit definierten Properties (hier waren Mal Werte)
+    const shapes = [];
+    
+    // Schlaufe für das mehrfache Erstellen des Cubes mit den oben definierten Properties
+    for (let c = 0; c < 10; c += 1) {
+      shapes.push( translate([0, 0, c * 14 + 15], cube({ size: 10 - c, center: [0, 0, 0] })) );
+      shapes.push( translate([0, 0, c * 14 + 15], arc({ radius: 10 - c +1, startAngle: 0, endAngle: Math.PI * 2, segments: 16 })) );
+    }
+
+
+
+  // when we are done we return one shape or an array of shapes
+  return [
+    coolerCyli,
+    coolerBla,
+    objects,
+    shapes
+  ];
+};
+
+// this lets javascript know what to execute once we import this
+module.exports = { main };
+
+
+
